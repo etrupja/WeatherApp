0fec8ffa81bdf55d847760cada988da120c3ea29
Ervis Trupja <ervistrupja@outlook.com>
2024-02-23 22:14:09 +0100

    Added a simple js call

--- WeatherApp/wwwroot/scripts/custom.js
+++ WeatherApp/wwwroot/scripts/custom.js
@@ -8,6 +8,9 @@
                 if (!res.ok) {
                     console.error(`HTTP error: ${res.status}`);
                 }
+
+                DotNet.invokeMethodAsync('WeatherApp', 'ShowFavoriteButton')
+
                 return res.json();
             })
             .then(data => resolve(data))
